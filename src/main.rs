37010d4b37884087c3571d86a938c73b3d391abc
Aaron <aaronzhouqian@ucla.edu>
2019-03-26 02:25:34 -0700

    generate genotype matrix from plink bed files

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,48 @@ fn extract_filename_arg(matches: &ArgMatches, arg_name: &str) -> String {
     }
 }
 
+struct SparsityStats {
+    sparsity_vec: Vec<f32>,
+}
+
+impl SparsityStats {
+    fn new(matrix: &Vec<Vec<u8>>) -> SparsityStats {
+        let mut sparsity_vec = Vec::new();
+        for snp_variants in matrix.iter() {
+            let num_zeros = snp_variants.iter().fold(0, |acc, &a| acc + (a == 0u8) as usize);
+            sparsity_vec.push(num_zeros as f32 / snp_variants.len() as f32);
+        }
+        SparsityStats { sparsity_vec }
+    }
+
+    fn histogram(&self, num_intervals: usize) -> Vec<f32> {
+        let mut histogram = vec![0; num_intervals + 1];
+        for s in self.sparsity_vec.iter() {
+            let index = (*s * num_intervals as f32) as usize;
+            histogram[index] += 1;
+        }
+        let mut ratio_distribution = vec![0f32; num_intervals + 1];
+        for i in 0..num_intervals + 1 {
+            ratio_distribution[i] = histogram[i] as f32 / self.sparsity_vec.len() as f32;
+        }
+        ratio_distribution
+    }
+
+    fn avg_sparsity(&self) -> f32 {
+        let mut iter = self.sparsity_vec.iter();
+        let mut current_mean = match iter.next() {
+            None => return 0f32,
+            Some(a) => *a
+        };
+
+        for (i, a) in iter.enumerate() {
+            let ratio = i as f32 / (i + 1) as f32;
+            current_mean = current_mean * ratio + a / (i + 1) as f32
+        }
+        current_mean
+    }
+}
+
 fn main() {
     let matches = clap_app!(Saber =>
         (version: "0.1")
@@ -30,8 +72,32 @@ fn main() {
     println!("PLINK bed filename: {}\nPLINK bim filename: {}\nPLINK fam filename: {}",
              plink_bed_filename, plink_bim_filename, plink_fam_filename);
 
-    match PlinkBed::new(&plink_bed_filename) {
-        Err(why) => { println!("failed to open {}\n{}", plink_bed_filename, why); }
-        Ok(_) => { println!("OK") }
+    let mut bed = match PlinkBed::new(&plink_bed_filename, &plink_bim_filename, &plink_fam_filename) {
+        Err(why) => {
+            println!("{}", why);
+            std::process::exit(1);
+        }
+        Ok(bed) => bed
+    };
+
+    println!("=> generating the genotype matrix");
+    let genotype_matrix = match bed.get_genotype_matrix() {
+        Err(io_error) => {
+            eprintln!("failed to get the genotype matrix: {}", io_error);
+            std::process::exit(1);
+        }
+        Ok(matrix) => matrix
+    };
+    println!("genotype_matrix.shape: ({}, {})", genotype_matrix.len(), genotype_matrix.first().unwrap_or(&Vec::new()).len());
+
+    let stats = SparsityStats::new(&genotype_matrix);
+    println!("avg sparsity: {}", stats.avg_sparsity());
+    let num_intervals = 20usize;
+    let histogram = stats.histogram(num_intervals);
+    let mut cum = 0f32;
+    let delta = 1f32 / num_intervals as f32;
+    for i in (0..num_intervals).rev() {
+        cum += histogram[i];
+        println!("[{:<3.2}, {:>3.2}): {:>6.2} {:>6.2}", i as f32 * delta, (i + 1) as f32 * delta, histogram[i], cum);
     }
 }
