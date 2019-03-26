2035387d3bf0ced88d4795a8ac8d159d20902f02
Aaron <aaronzhouqian@ucla.edu>
2019-03-25 17:58:28 -0700

    plink bed error enum

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 #[macro_use]
 extern crate clap;
 
+use clap::ArgMatches;
 use bio_file_reader::plink_bed::PlinkBed;
+
+fn extract_filename_arg(matches: &ArgMatches, arg_name: &str) -> String {
+    match matches.value_of(arg_name) {
+        Some(filename) => filename.to_string(),
+        None => {
+            eprintln!("the argument {} is required", arg_name);
+            std::process::exit(1);
+        }
+    }
+}
 
 fn main() {
     let matches = clap_app!(Saber =>
-    (version: "0.1")
-    (author: "Aaorn Zhou")
-    (@arg plink_bed_filename: +required)
+        (version: "0.1")
+        (author: "Aaron Zhou")
+        (@arg plink_bed_filename: --bed <BED> "required")
+        (@arg plink_bim_filename: --bim <BIM> "required")
+        (@arg plink_fam_filename: --fam <FAM> "required")
     ).get_matches();
 
-    let plink_bed_filename = match matches.value_of("plink_bed_filename") {
-        Some(plink_bed_filename) => plink_bed_filename.to_string(),
-        None => {
-            eprintln!("the argument plink_bed_filename is required");
-            std::process::exit(1);
-        }
-    };
+    let plink_bed_filename = extract_filename_arg(&matches, "plink_bed_filename");
+    let plink_bim_filename = extract_filename_arg(&matches, "plink_bim_filename");
+    let plink_fam_filename = extract_filename_arg(&matches, "plink_fam_filename");
 
-    println!("input filename: {}", plink_bed_filename);
+    println!("PLINK bed filename: {}\nPLINK bim filename: {}\nPLINK fam filename: {}",
+             plink_bed_filename, plink_bim_filename, plink_fam_filename);
 
     match PlinkBed::new(&plink_bed_filename) {
-        Err(why) => { println!("{}", why); }
+        Err(why) => { println!("failed to open {}\n{}", plink_bed_filename, why); }
         Ok(_) => { println!("OK") }
     }
 }
