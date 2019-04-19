14070008f92f413975373c6e0cbac401d2b5740a
Aaron <aaronzhouqian@ucla.edu>
2019-04-19 01:24:09 -0700

    tr kk sublinear time estimator

--- src/bin/test_gg_trace_estimates.rs
+++ src/bin/test_gg_trace_estimates.rs
@@ -21,7 +21,7 @@ use estimate_heritability_cublas as estimate_heritability;
 use saber::program_flow::OrExit;
 use saber::timer::Timer;
 
-use saber::gg_estimates::estimate_gxg_trace;
+use saber::gg_estimates::{estimate_gxg_gram_trace, estimate_kk_trace};
 
 fn extract_filename_arg(matches: &ArgMatches, arg_name: &str) -> String {
     match matches.value_of(arg_name) {
@@ -77,34 +77,51 @@ fn main() {
     let gxg = get_gxg_arr(&geno_arr);
     println!("GxG dim: {:?}", gxg.dim());
 
-    let ones = Array::<f32, Ix1>::ones(gxg.dim().1);
+    let mut timer = Timer::new();
     println!("\n=> calculating tr_k_true");
-    let tr_k_true = (&gxg * &gxg).dot(&ones).sum() as f64;
+    let tr_k_true = (&gxg * &gxg).dot(&Array::<f32, Ix1>::ones(gxg.dim().1)).sum() as f64;
     println!("tr_k_true: {}", tr_k_true);
+    timer.print();
 
     println!("\n=> estimating the trace of GxG.dot(GxG.T)");
     let mut ratio_list = Vec::new();
     for iter in 0..50 {
-        let tr_k_est;
-        match estimate_gxg_trace(&geno_arr, num_random_vecs) {
-            Ok(t) => tr_k_est = t,
+        let tr_k_est = match estimate_gxg_gram_trace(&geno_arr, num_random_vecs) {
+            Ok(t) => t,
             Err(why) => {
                 eprintln!("{}", why);
-                return ();
+                return;
             }
         };
         let r = (tr_k_est - tr_k_true) / tr_k_true;
         println!("\niter: {} tr_k_est: {}\nk_trace_est error ratio: {:.5}", iter + 1, tr_k_est, r);
         ratio_list.push(r);
     }
+    timer.print();
 
     let ratio_avg = ratio_list.iter().sum::<f64>() / ratio_list.len() as f64;
     let ratio_std = (ratio_list.iter().map(|r| (r - ratio_avg) * (r - ratio_avg)).sum::<f64>() / ratio_list.len() as f64).sqrt();
     println!("\nratio_avg: {}\nratio_std: {}", ratio_avg, ratio_std);
 
-//    println!("naive trace computation for KK");
-//    let k = gxg.dot(&gxg.t());
-//    println!("k dim: {:?}", k.dim());
-//    let true_trace = sum_of_squares(k.iter());
-//    println!("kk true trace: {}", true_trace);
+    println!("\n=> computing tr_kk_est");
+    let tr_kk_est = match estimate_kk_trace(&geno_arr, num_random_vecs) {
+        Ok(t) => t,
+        Err(why) => {
+            eprintln!("{}", why);
+            return;
+        }
+    };
+    println!("tr_kk_est: {}", tr_kk_est);
+    timer.print();
+
+    println!("\n=> calculating tr_kk_true");
+    let k = gxg.dot(&gxg.t());
+    println!("k dim: {:?}", k.dim());
+    let tr_kk_true = (&k * &k).dot(&Array::<f32, Ix1>::ones(k.dim().1)).sum() as f64;
+    println!("tr_kk_true: {}", tr_kk_true);
+    timer.print();
+
+    let kk_ratio = (tr_kk_est - tr_kk_true) / tr_kk_true;
+    println!("tr kk error ratio: {}", kk_ratio);
+
 }
