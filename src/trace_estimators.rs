3f0f3d2a5c7aca7023313e0d5b625fb7ad6beaf8
Aaron <aaronzhouqian@ucla.edu>
2019-05-26 15:34:02 -0700

    parallelize trace estimators

--- src/trace_estimators.rs
+++ src/trace_estimators.rs
@@ -1,4 +1,8 @@
-use ndarray::{Array, Ix1, Ix2};
+extern crate ndarray_parallel;
+
+use ndarray_parallel::prelude::*;
+
+use ndarray::{Array, Axis, Ix1, Ix2};
 use crate::matrix_util::generate_plus_minus_one_bernoulli_matrix;
 use crate::stats_util::sum_of_squares;
 
@@ -32,13 +36,24 @@ pub fn estimate_gxg_gram_trace(geno_arr: &Array<f32, Ix2>, num_random_vecs: usiz
     let geno_ssq = (geno_arr * geno_arr).dot(&ones);
     let squashed = geno_arr.dot(&u_arr);
     let squashed_squared = &squashed * &squashed;
-    let mut sum = 0f64;
-    for col in squashed_squared.gencolumns() {
-        let uugg = (&col - &geno_ssq) / 2.;
-        sum += sum_of_squares(uugg.iter());
-    }
-    let avg = sum / num_random_vecs as f64;
-    Ok(avg)
+
+    let mut sums = Vec::new();
+    squashed_squared.axis_iter(Axis(1))
+        .into_par_iter()
+        .map(|col|{
+            let uugg = (&col - &geno_ssq) / 2.;
+            sum_of_squares(uugg.iter())
+        })
+        .collect_into_vec(&mut sums);
+    Ok(sums.iter().sum::<f64>() / num_random_vecs as f64)
+
+//    let mut sum = 0f64;
+//    for col in squashed_squared.gencolumns() {
+//        let uugg = (&col - &geno_ssq) / 2.;
+//        sum += sum_of_squares(uugg.iter());
+//    }
+//    let avg = sum / num_random_vecs as f64;
+//    Ok(avg)
 }
 
 pub fn estimate_gxg_kk_trace(geno_arr: &Array<f32, Ix2>, num_random_vecs: usize) -> Result<f64, String> {
@@ -50,26 +65,36 @@ pub fn estimate_gxg_kk_trace(geno_arr: &Array<f32, Ix2>, num_random_vecs: usize)
     let geno_ssq = gg_sq.dot(&ones);
     let squashed = geno_arr.dot(&u_arr);
     let squashed_squared = &squashed * &squashed;
-    let mut sum = 0f64;
     let num_rand_z_vecs = 100;
     println!("num_rand_z_vecs: {}\nnum_random_vecs: {}", num_rand_z_vecs, num_random_vecs);
 
-    let mut i = 0;
-    for col in squashed_squared.gencolumns() {
-        if i % 100 == 0 {
-            println!("{} / {}", i + 1, num_random_vecs);
-        }
-        i += 1;
-        let uugg_sum = (&col - &geno_ssq) / 2.;
-        let wg = &geno_arr.t() * &uugg_sum;
-        let s = (&gg_sq.t() * &uugg_sum).sum();
-        let rand_vecs = generate_plus_minus_one_bernoulli_matrix(num_cols, num_rand_z_vecs);
-        let geno_arr_dot_rand_vecs = geno_arr.dot(&rand_vecs);
-        let ggz = wg.dot(&geno_arr_dot_rand_vecs);
-        sum += (((&ggz * &ggz).sum() / num_rand_z_vecs as f32 - s) / 2.) as f64;
-    }
-    let avg = sum / num_random_vecs as f64;
-    Ok(avg)
+    let mut sums = Vec::new();
+    squashed_squared.axis_iter(Axis(1))
+        .into_par_iter()
+        .map(|col|{
+            let uugg_sum = (&col - &geno_ssq) / 2.;
+            let wg = &geno_arr.t() * &uugg_sum;
+            let s = (&gg_sq.t() * &uugg_sum).sum();
+            let rand_vecs = generate_plus_minus_one_bernoulli_matrix(num_cols, num_rand_z_vecs);
+            let geno_arr_dot_rand_vecs = geno_arr.dot(&rand_vecs);
+            let ggz = wg.dot(&geno_arr_dot_rand_vecs);
+            (((&ggz * &ggz).sum() / num_rand_z_vecs as f32 - s) / 2.) as f64
+        })
+        .collect_into_vec(&mut sums);
+    Ok(sums.iter().sum::<f64>() / num_random_vecs as f64)
+
+//    let mut sum = 0f64;
+//    for col in squashed_squared.gencolumns() {
+//        let uugg_sum = (&col - &geno_ssq) / 2.;
+//        let wg = &geno_arr.t() * &uugg_sum;
+//        let s = (&gg_sq.t() * &uugg_sum).sum();
+//        let rand_vecs = generate_plus_minus_one_bernoulli_matrix(num_cols, num_rand_z_vecs);
+//        let geno_arr_dot_rand_vecs = geno_arr.dot(&rand_vecs);
+//        let ggz = wg.dot(&geno_arr_dot_rand_vecs);
+//        sum += (((&ggz * &ggz).sum() / num_rand_z_vecs as f32 - s) / 2.) as f64;
+//    }
+//    let avg = sum / num_random_vecs as f64;
+//    Ok(avg)
 }
 
 pub fn estimate_gxg_dot_y_norm_sq(geno_arr: &Array<f32, Ix2>, y: &Array<f32, Ix1>, num_random_vecs: usize) -> f64 {
