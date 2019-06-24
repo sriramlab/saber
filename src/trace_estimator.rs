0adc62373d43298925879e9de1199fbb141fdddd
Aaron <aaronzhouqian@ucla.edu>
2019-06-23 23:37:59 -0700

    change chunk size to 100

--- src/trace_estimator.rs
+++ src/trace_estimator.rs
@@ -12,7 +12,7 @@ pub fn estimate_tr_kk(geno_arr_bed: &mut PlinkBed, num_random_vecs: usize) -> f6
     let rand_mat = generate_plus_minus_one_bernoulli_matrix(num_people, num_random_vecs);
 
     use rayon::prelude::*;
-    let chunk_size = 5000;
+    let chunk_size = 100;
     let xxz_arr: Vec<f32> = geno_arr_bed
         .col_chunk_iter(chunk_size)
         .into_par_iter()
@@ -24,8 +24,8 @@ pub fn estimate_tr_kk(geno_arr_bed: &mut PlinkBed, num_random_vecs: usize) -> f6
             }
             acc
         })
-        .reduce(|| vec![0f32; num_people * num_random_vecs], |mut a, b|{
-            for (i, val) in b.iter().enumerate(){
+        .reduce(|| vec![0f32; num_people * num_random_vecs], |mut a, b| {
+            for (i, val) in b.iter().enumerate() {
                 a[i] += val;
             }
             a
