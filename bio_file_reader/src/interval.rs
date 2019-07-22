4184ab87acb90ac74e90ce311e5da5d40d0c5166
Aaron <aaronzhouqian@ucla.edu>
2019-07-21 19:38:22 -0700

    interval implementations

--- bio_file_reader/src/interval.rs
+++ bio_file_reader/src/interval.rs
@@ -1,10 +1,16 @@
+use std::cmp::{max, min};
 use std::fmt;
 
 use num::integer::Integer;
 use num::Num;
 use num::traits::NumAssignOps;
 
-pub mod traits;
+pub mod trait_impl;
+
+pub trait MergeIntervals<I: Interval<Element=E>, E: Num + Copy> {
+    fn sort_and_coalesce_intervals(&self) -> Vec<I>;
+    fn sort_and_coalesce_intervals_inplace(&mut self);
+}
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum IntervalShape {
@@ -14,81 +20,137 @@ pub enum IntervalShape {
     LeftClosedRightOpen,
 }
 
-#[derive(Copy, Clone)]
-pub struct Interval<T> {
-    start: T,
-    end: T,
-    shape: IntervalShape,
-}
+pub trait Interval {
+    type Element: Num + Copy;
 
-impl<T: Num + Copy> Interval<T> {
-    pub fn new(start: T, end: T, shape: IntervalShape) -> Interval<T> {
-        Interval {
-            start,
-            end,
-            shape,
-        }
-    }
+    fn get_start(&self) -> Self::Element;
 
-    pub fn len(&self) -> T {
-        self.end - self.start
+    fn set_start(&mut self, start: Self::Element);
+
+    fn get_end(&self) -> Self::Element;
+
+    fn set_end(&mut self, end: Self::Element);
+
+    fn get_shape(&self) -> IntervalShape;
+
+    fn len(&self) -> Self::Element {
+        self.get_end() - self.get_start()
     }
 }
 
-impl<T: Num> PartialEq for Interval<T> {
-    fn eq(&self, other: &Interval<T>) -> bool {
-        self.start == other.start && self.end == other.end && self.shape == other.shape
+impl<T: Num + Copy> PartialEq for dyn Interval<Element=T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.get_start() == other.get_start() && self.get_end() == other.get_end() && self.get_shape() == other.get_shape()
     }
 }
 
-impl<T: fmt::Display> fmt::Display for Interval<T> {
+impl<T: Num + Copy + fmt::Display> fmt::Display for dyn Interval<Element=T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.shape {
-            IntervalShape::Open => write!(f, "({}, {})", self.start, self.end),
-            IntervalShape::Closed => write!(f, "[{}, {}]", self.start, self.end),
-            IntervalShape::LeftOpenRightClosed => write!(f, "({}, {}]", self.start, self.end),
-            IntervalShape::LeftClosedRightOpen => write!(f, "[{}, {})", self.start, self.end)
+        match self.get_shape() {
+            IntervalShape::Open => write!(f, "({}, {})", self.get_start(), self.get_end()),
+            IntervalShape::Closed => write!(f, "[{}, {}]", self.get_start(), self.get_end()),
+            IntervalShape::LeftOpenRightClosed => write!(f, "({}, {}]", self.get_start(), self.get_end()),
+            IntervalShape::LeftClosedRightOpen => write!(f, "[{}, {})", self.get_start(), self.get_end())
         }
     }
 }
 
-impl<T: fmt::Display> fmt::Debug for Interval<T> {
+impl<T: Num + Copy + fmt::Display> fmt::Debug for dyn Interval<Element=T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self)
     }
 }
 
-pub struct ClosedIntegerIntervalCollector<T: Integer> {
-    intervals: Vec<Interval<T>>,
+#[derive(Clone, Copy, PartialEq)]
+pub struct IntervalOverIntegers<T: Integer + Copy> {
+    start: T,
+    end: T,
 }
 
-impl<T: Integer + NumAssignOps + Copy + fmt::Display> ClosedIntegerIntervalCollector<T> {
-    pub fn new() -> ClosedIntegerIntervalCollector<T> {
-        ClosedIntegerIntervalCollector {
-            intervals: Vec::<Interval<T>>::new(),
+impl<E: Integer + Copy> IntervalOverIntegers<E> {
+    pub fn new(start: E, end: E) -> IntervalOverIntegers<E> {
+        IntervalOverIntegers {
+            start,
+            end,
         }
     }
 
-    pub fn get_intervals_ref(&self) -> &Vec<Interval<T>> {
+    pub fn coalesce_with(&self, other: &IntervalOverIntegers<E>) -> Option<IntervalOverIntegers<E>> {
+        if self.start > other.end + E::one() || self.end + E::one() < other.start {
+            None
+        } else {
+            Some(IntervalOverIntegers::new(min(self.start, other.start), max(self.end, other.end)))
+        }
+    }
+}
+
+impl<E: Integer + Copy> Interval for IntervalOverIntegers<E> {
+    type Element = E;
+
+    fn get_start(&self) -> E {
+        self.start
+    }
+
+    fn set_start(&mut self, start: E) {
+        self.start = start;
+    }
+
+    fn get_end(&self) -> E {
+        self.end
+    }
+
+    fn set_end(&mut self, end: E) {
+        self.end = end;
+    }
+
+    fn get_shape(&self) -> IntervalShape {
+        IntervalShape::Closed
+    }
+}
+
+impl<E: Integer + Copy + fmt::Display + 'static> fmt::Display for IntervalOverIntegers<E>  {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self as &dyn Interval<Element=E>)
+    }
+}
+
+impl<E: Integer + Copy + fmt::Display + 'static> fmt::Debug for IntervalOverIntegers<E>  {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self as &dyn Interval<Element=E>)
+    }
+}
+
+pub struct ClosedIntegerIntervalCollector<E: Integer + Copy> {
+    intervals: Vec<IntervalOverIntegers<E>>
+}
+
+impl<E: Integer + NumAssignOps + Copy + fmt::Display> ClosedIntegerIntervalCollector<E> {
+    pub fn new() -> ClosedIntegerIntervalCollector<E> {
+        ClosedIntegerIntervalCollector {
+            intervals: Vec::<IntervalOverIntegers<E>>::new(),
+        }
+    }
+
+    pub fn get_intervals_ref(&self) -> &Vec<IntervalOverIntegers<E>> {
         &self.intervals
     }
 
-    pub fn get_intervals(self) -> Vec<Interval<T>> {
+    pub fn get_intervals(self) -> Vec<IntervalOverIntegers<E>> {
         self.intervals
     }
 
-    pub fn append_larger_point(&mut self, point: T) -> Result<(), String> {
+    pub fn append_larger_point(&mut self, point: E) -> Result<(), String> {
         match self.intervals.last_mut() {
             None => {
-                self.intervals.push(Interval::new(point, point, IntervalShape::Closed));
+                self.intervals.push(IntervalOverIntegers::new(point, point));
             }
             Some(interval) => {
                 if interval.end > point {
                     return Err(format!("The last encountered point {} is larger than the new point {} to be collected", interval.end, point));
-                } else if interval.end + T::one() == point {
+                } else if interval.end + E::one() == point {
                     interval.end = point;
                 } else {
-                    self.intervals.push(Interval::new(point, point, IntervalShape::Closed));
+                    self.intervals.push(IntervalOverIntegers::new(point, point));
                 }
             }
         }
@@ -98,7 +160,7 @@ impl<T: Integer + NumAssignOps + Copy + fmt::Display> ClosedIntegerIntervalColle
 
 #[cfg(test)]
 mod tests {
-    use super::{ClosedIntegerIntervalCollector, Interval, IntervalShape};
+    use super::{ClosedIntegerIntervalCollector, IntervalOverIntegers};
 
     #[test]
     fn test_append_larger_point() {
@@ -110,9 +172,26 @@ mod tests {
         collector.append_larger_point(8).unwrap();
         collector.append_larger_point(9).unwrap();
         assert_eq!(collector.get_intervals(), vec![
-            Interval::new(1, 1, IntervalShape::Closed),
-            Interval::new(4, 5, IntervalShape::Closed),
-            Interval::new(7, 9, IntervalShape::Closed)
+            IntervalOverIntegers::new(1, 1),
+            IntervalOverIntegers::new(4, 5),
+            IntervalOverIntegers::new(7, 9)
         ]);
+    }
+
+    #[test]
+    fn test_coalesce_with() {
+        fn test(a: i32, b: i32, c: i32, d: i32, expected: Option<IntervalOverIntegers<i32>>) {
+            let i1 = IntervalOverIntegers::new(a, b);
+            let i2 = IntervalOverIntegers::new(c, d);
+            let m1 = i1.coalesce_with(&i2);
+            let m2 = i2.coalesce_with(&i1);
+            assert_eq!(m1, m2);
+            assert_eq!(m1, expected);
+        }
+        test(1, 3, 4, 5, Some(IntervalOverIntegers::new(1, 5)));
+        test(2, 3, 0, 5, Some(IntervalOverIntegers::new(0, 5)));
+        test(2, 5, 1, 3, Some(IntervalOverIntegers::new(1, 5)));
+        test(-3, -1, -1, 2, Some(IntervalOverIntegers::new(-3, 2)));
+        test(3, 5, 7, 9, None);
     }
 }
--- src/bin/leave_one_chrom_out.rs
+++ src/bin/leave_one_chrom_out.rs
@@ -1,25 +1,15 @@
 use std::fs::OpenOptions;
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{BufWriter, Write};
 
 use clap::{Arg, clap_app};
 
+use bio_file_reader::interval::{Interval, IntervalOverIntegers, MergeIntervals};
 use bio_file_reader::plink_bed::PlinkBed;
+use bio_file_reader::plink_bim::PlinkBim;
 use saber::program_flow::OrExit;
 use saber::util::{extract_str_arg, get_bed_bim_fam_path};
-use bio_file_reader::plink_bim::PlinkBim;
-use bio_file_reader::interval::traits::MergeIntervals;
-use bio_file_reader::interval::Interval;
 
 const CHUNK_SIZE: usize = 1024;
-
-fn get_chrom_sizes(chrom_size_filepath: &String) -> Result<Vec<usize>, String> {
-    let buf = match OpenOptions::new().read(true).open(chrom_size_filepath.as_str()) {
-        Err(why) => return Err(format!("failed to open {}: {}", chrom_size_filepath, why)),
-        Ok(f) => BufReader::new(f)
-    };
-    let count_vec: Vec<usize> = buf.lines().map(|l| l.unwrap().parse::<usize>().unwrap()).collect();
-    Ok(count_vec)
-}
 
 fn main() {
     let mut app = clap_app!(leave_one_chrom_out =>
@@ -36,20 +26,9 @@ fn main() {
             .required(true)
             .help("output path prefix. /PATH/PREFIX will generate files named /PATH/PREFIX_minus_chrom_{CHROM} etc.")
     );
-
-//    app = app.arg(
-//        Arg::with_name("chrom_size_file")
-//            .long("chrom-size-file")
-//            .short("c")
-//            .required(true)
-//            .takes_value(true)
-//            .help("the file path to a file containing the chromosome sizes, \
-//            where the n-th line is the number of SNPs in the n-th chromosome")
-//    );
     let matches = app.get_matches();
 
     let bfile = extract_str_arg(&matches, "bfile");
-//    let chrom_size_filepath = extract_str_arg(&matches, "chrom_size_file");
     let out_path_prefix = extract_str_arg(&matches, "out_prefix");
 
     let [bed_path, bim_path, fam_path] = get_bed_bim_fam_path(&bfile);
@@ -62,45 +41,39 @@ fn main() {
     let chrom_to_fileline_positions = bim.get_chrom_to_fileline_positions()
                                          .unwrap_or_exit(Some("failed to get the file-line position map"));
 
+    let num_bytes_per_snp = bed.num_bytes_per_snp;
+    let magic_bytes = PlinkBed::get_magic_bytes();
+
     let chrom_list: Vec<String> = chrom_to_fileline_positions.keys().map(|k| k.to_string()).collect();
     for excluded_chrom in chrom_list.iter() {
-        let merged_positions: Vec<Interval<usize>> = chrom_to_fileline_positions
-            .iter()
-            .filter(|(chrom, positions)| *chrom != excluded_chrom)
-            // intra-chromosome coalescence
-            .flat_map(|(chrom, positions)| positions.sort_and_coalesce_intervals())
-            .collect::<Vec<Interval<usize>>>()
-            // inter-chromosome coalescence
-            .sort_and_coalesce_intervals();
-        println!("excluded {}: {:?}", excluded_chrom, merged_positions);
-    }
+        let out_filepath = format!("{}_minus_chrom_{}", out_path_prefix, excluded_chrom);
+        println!("\n=> writing to file: {}", out_filepath);
+        let mut buf_writer = BufWriter::new(OpenOptions::new()
+            .create(true).truncate(true).write(true)
+            .open(&out_filepath)
+            .unwrap_or_exit(None::<String>));
 
-//    let chrom_sizes = get_chrom_sizes(&chrom_size_filepath).unwrap();
-//
-//    let num_bytes_per_snp = bed.num_bytes_per_snp;
-//    let magic_bytes = PlinkBed::get_magic_bytes();
-//    for index_to_omit in 0..chrom_sizes.len() {
-//        let out_filepath = format!("{}_minus_chrom_{}", out_path_prefix, index_to_omit + 1);
-//        println!("=> writing to file: {}", out_filepath);
-//        let mut buf_writer = BufWriter::new(OpenOptions::new()
-//            .create(true).truncate(true).write(true)
-//            .open(&out_filepath)
-//            .unwrap_or_exit(None::<String>));
-//
-//        buf_writer.write(&magic_bytes).unwrap_or_exit(Some(format!("failed to write to file {}", out_filepath)));
-//        let mut current_byte_index = magic_bytes.len();
-//        for (i, num_snps) in chrom_sizes.iter().enumerate() {
-//            let num_bytes = num_snps * num_bytes_per_snp;
-//            for chunk in bed.byte_chunk_iter(current_byte_index,
-//                                             current_byte_index + num_bytes,
-//                                             CHUNK_SIZE)
-//                            .unwrap_or_exit(Some("failed to create byte_chunk_iter")) {
-//                if i != index_to_omit {
-//                    buf_writer.write(chunk.as_slice())
-//                              .unwrap_or_exit(Some(format!("failed to write to file {}", out_filepath)));
-//                }
-//            }
-//            current_byte_index += num_bytes;
-//        }
-//    }
+        buf_writer.write(&magic_bytes).unwrap_or_exit(Some(format!("failed to write to file {}", out_filepath)));
+
+        let mut snp_intervals: Vec<IntervalOverIntegers<usize>> = chrom_to_fileline_positions
+            .iter()
+            .filter(|(chrom, _positions)| *chrom != excluded_chrom)
+            // intra-chromosome coalescence
+            .flat_map(|(_chrom, positions)| positions.sort_and_coalesce_intervals())
+            .collect::<Vec<IntervalOverIntegers<usize>>>();
+        // inter-chromosome coalescence
+        snp_intervals.sort_and_coalesce_intervals_inplace();
+        println!("Excluding chromosome {}. Using file lines {:?}", excluded_chrom, snp_intervals);
+
+        for interval in snp_intervals.into_iter() {
+            let start_byte_index = magic_bytes.len() + num_bytes_per_snp * interval.get_start();
+            let end_byte_index = magic_bytes.len() + num_bytes_per_snp * interval.get_end();
+
+            for chunk in bed.byte_chunk_iter(start_byte_index, end_byte_index, CHUNK_SIZE)
+                            .unwrap_or_exit(Some("failed to create byte_chunk_iter")) {
+                buf_writer.write(chunk.as_slice())
+                          .unwrap_or_exit(Some(format!("failed to write to file {}", out_filepath)));
+            }
+        }
+    }
 }
