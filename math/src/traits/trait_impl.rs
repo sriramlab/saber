99965d95f1b413db0486b97558f3d8d59cfa5580
Aaron <aaronzhouqian@ucla.edu>
2019-07-26 16:22:11 -0700

    refactor implementation on foreign traits

--- math/src/traits/trait_impl.rs
+++ math/src/traits/trait_impl.rs
@@ -1,5 +1,8 @@
-use crate::traits::{Collecting, Constructable};
 use std::ops::Deref;
+use std::slice::Iter;
+
+use crate::traits::{Collecting, Constructable};
+use crate::traits::ToIterator;
 
 impl<T> Constructable for Vec<T> {
     fn new() -> Vec<T> {
@@ -16,5 +19,11 @@ impl<T> Collecting<T> for Vec<T> {
 impl<'a, T: Clone> Collecting<&'a T> for Vec<T> where &'a T: Deref {
     fn collect(&mut self, item: &'a T) {
         self.push((*item).clone());
+    }
+}
+
+impl<'a, 's: 'a, E> ToIterator<'s, Iter<'a, E>, &'a E> for Vec<E> {
+    fn to_iter(&'s self) -> Iter<'a, E> {
+        self.iter()
     }
 }
